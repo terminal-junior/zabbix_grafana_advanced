afc6239857ad5982152b4175d8e0c6951e741f02
Junior <jj.josijunior@gmail.com>
2025-12-09 12:16:52 -0300

    Update zabbix-proxy-externo.sh

--- Scripts/zabbix-proxy-externo.sh
+++ Scripts/zabbix-proxy-externo.sh
@@ -5,6 +5,8 @@
 # Rocky Linux 9 + PostgreSQL
 # -----------------------------------------------
 
+hostnamectl set-hostname Zabbix-Proxy-Externo
+
 echo "=== Atualizando o sistema e instalando pacotes básicos ==="
 dnf update -y &>/dev/null
 dnf install -y nano openssh-server firewalld &>/dev/null
